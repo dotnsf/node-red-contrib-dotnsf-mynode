315f6d12c069baab7ad940ef0bcbdd48eaf04683
K.Kimura <dotnsf@jp.ibm.com>
2017-08-17 02:47:05 +0000

    fix for #2

--- hash/hash.js
+++ hash/hash.js
@@ -1,12 +1,21 @@
 var crypto = require( 'crypto' );
-var hash = crypto.createHash( 'sha512' );
 
 module.exports = function( RED ){
   function HashNode( config ){
     RED.nodes.createNode( this, config );
+
+    this.algorhythm = config.algorhythm;
+    console.log( this.algorhythm );
+    var hash = crypto.createHash( this.algorhythm );
+
     var node = this;
     node.on( 'input', function( msg ){
       var payload = msg.payload;
+      var algorhythm = msg.algorhythm;
+      if( algorhythm ){
+        console.log( algorhythm );
+        hash = crypto.createHash( algorhythm );
+      }
 
       hash.update( payload );
       msg.hash = hash.digest( 'hex' );
